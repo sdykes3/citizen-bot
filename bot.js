01ca671046a89937db023ca8ba1107df1b2325a7
Stephanie <stephanie.n.dykes@gmail.com>
2016-12-05 11:09:42 -0500

    Update bot with feedback iteration; daily updates, asks for consent in accordance to Twitter guidelines, updated website

--- bot.js
+++ bot.js
@@ -13,11 +13,12 @@ var timeToTweet2 = true;
 var timeToTweet3 = true;
 
 // filters for stream searchs; commas act as logical OR ; space acts as logical AND (order doesnt matter)
-var capTrack = 'hate capitalism, capitalism ruining, capitalism sucks, capitalism broken, capitalism';
+var capTrack = 'hate capitalism, capitalism ruining, capitalism sucks, capitalism broken';
 var demTrack = 'hate democracy, democracy is broken, failure of democracy, democracy joke, facist america, I burned a US flag';
 
 // pre-written tweets for bot to post
 var tweets = ["Congratulations to our Patriot Partner™ rewards members! Keep up the great work that makes our nation thrive!",
+				"Consider enrolling in our feedback program to know when your points change online, without having to log in! Simply reply here with 'YES'",
 				"Land of the free, home of the brave!",
 				"Remember to check in at select retailers to gain bonus Patriot Points!",
 				"Lower interest rates, higher application approval, priority jobs; Being a Patriot pays!",
@@ -34,6 +35,14 @@ var tweets = ["Congratulations to our Patriot Partner™ rewards members! Keep u
 var tweetIndex = 0; // current index of the aboe tweet array; will tweet each before repeating
 
 
+//map with user.screen_name as key, value as consent (null or "yes" or "no")
+var map = {};
+
+//keep track of negative things bot could respond to
+var weatherCount = 0;
+//keep track of how many time interval call has been made, as way of keeping time
+var intervalCount = 0;
+
 
 // Stream - Replies to users who tweet @ me
 var streamReply = T.stream('statuses/filter', {track: '@PatriotPointUSA'});
@@ -43,7 +52,7 @@ streamReply.on('tweet', function (tweet) {
 	var name = '@' + tweet.user.screen_name;
 	console.log(name);
 
-	//Now follow that user
+	//Follow that user
 	T.post('friendships/create', {screen_name: name }, function (err, reply) {
 		if (err != null) {
 			console.log('Error: ', err);
@@ -53,14 +62,32 @@ streamReply.on('tweet', function (tweet) {
 		}
 	});
 
-	var respondText = "We hear your concerns. Please check the website for details about your point deductions and privileges.";
+	//If a consent response
+	if(tweet.text == "@PatriotPointUSA yes" || tweet.text == "@PatriotPointUSA YES" || tweet.text == "@PatriotPointUSA Yes") {
+		//add as consenting to the user map
+		console.log('Added consent of ' + tweet.user.screen_name);
+		map[tweet.user.screen_name] = "yes";
 
-	if (debug) {
-		console.log(tweet.text);
-	} else {
-		// Respond to mention
-		respond(respondText, name, nameID);
+		//ideally, go back and find their first tweet and respond to it
+		//or track them especially closely now in the stream (how?)
+
+	} else if (tweet.text == "@PatriotPointUSA stop" || tweet.text == "@PatriotPointUSA STOP" || tweet.text == "@PatriotPointUSA Stop") {
+		//if opting out
+		console.log('Removed consent of ' + tweet.user.screen_name);
+		var respondText = "We hear your concerns. You have been opted out of the feedback program.";
+		map[tweet.user.screen_name] = "no";
+	}  else {
+		//if responding with something other than "yes"; both new and old users
+		var respondText = "We hear your concerns. Please check the website for details about your point deductions and privileges.";
+
+		if (debug) {
+			console.log(tweet.text);
+		} else {
+			// Respond to mention
+			respond(respondText, name, nameID);
+		}
 	}
+
 })
 
 
@@ -72,7 +99,7 @@ stream1.on('tweet', function (tweet) {
 	console.log("capitalismSearch");
 
 	if(timeToTweet1) {
-		respondLatest(capitalismSearch);
+		getConsent(capitalismSearch);
 		timeToTweet1 = false; // wait before tweeting on this topic again, reset in runBot()
 	}
 })
@@ -83,7 +110,7 @@ stream2.on('tweet', function (tweet) {
 	console.log("democracySearch");
 
 	if(timeToTweet2) {
-		respondLatest(democracySearch);
+		getConsent(democracySearch);
 		timeToTweet2 = false; // wait before tweeting on this topic again, reset in runBot()
 	}
 })
@@ -94,7 +121,7 @@ stream3.on('tweet', function (tweet) {
 	console.log("corruptSearch");
 
 	if(timeToTweet3) {
-		respondLatest(corruptSearch);
+		getConsent(corruptSearch);
 		timeToTweet3 = false; // wait before tweeting on this topic again, reset in runBot()
 	}
 })
@@ -114,9 +141,32 @@ function respond(data, name, nameID) {
 }
 
 
+// Responds when called by stream
+function getConsent(chosenSearch) {
+	if(map[chosenSearch.user.screen_name] == null) {
+		//no consent, must ask
+		console.log("No prior consent; ask");
+
+		var fullResponse = "Hello citizen; If you would like to enroll in our new real-time Patriot Point feedback program, please respond with 'YES'";
+		var nameID = chosenSearch.id_str;
+		var name = '@' + chosenSearch.user.screen_name;
+		console.log(name);
+
+		respond(fullResponse, name, nameID);
+		//added to map once they reply (in the stream that tracks @PatriotPointUSA)
+	} else if(map[chosenSearch.user.screen_name] == "yes") {
+		//have prior consent, can respond
+		console.log("Yes prior consent; pass to respondeWithConsent");
+		respondWithConsent(chosenSearch);
+	} else if(map[chosenSearch.user.screen_name] == "no") {
+		//previous had consent, then opted out; do nothing
+	}
+
+}
+
 
 // Responds when called by stream
-function respondLatest(chosenSearch) {
+function respondWithConsent(chosenSearch) {
 	// Partial responses to people, telling them why they lost points
 	var respondText1 = "ATTENTION: You have lost "
 	var respondText2 = " points for ";
@@ -248,6 +298,32 @@ function tweet() {
 	}
 }
 
+// Tweet positivity "weather" update dailt
+function weather(negativity) {
+	var tweetText = "";
+
+	if(negativity == "high") {
+		tweetText = "Lots of negativity recently; Stay strong, and be mindful of what truly matters. We are all so lucky to be in this great and free nation!";
+	} else if(negativity == "med") {
+		tweetText = "Be wary of how you post; Consider enrolling in our feedback program to know when your points change online, without the need to log in!";
+	} else if(negativity == "low") {
+		tweetText = "Lots of love for America today! Keep up the great work, fellow Patriots!";
+	}
+
+	if (debug) {
+		console.log(tweetText);
+	} else {
+		T.post('statuses/update', {status: tweetText }, function(err, reply) {
+			if (err != null) {
+				console.log('Error: ', err);
+			}
+			else {
+				console.log('Tweeted: ', tweetText);
+			}
+		});
+	}
+}
+
 
 // Returns a random integer between min (included) and max (excluded), for points
 function getRandomInt(min, max) {
@@ -263,7 +339,7 @@ function runBot() {
 
 	var rand = Math.random();
 	// 50/50 chance of tweeting premade string or retweeting something semi-random
-	if(rand >= .50) {
+	if(rand >= .70) {
 		tweet();
 	} else {
 		retweetLatest();
@@ -272,9 +348,30 @@ function runBot() {
 	timeToTweet1 = true;
 	timeToTweet2 = true;
 	timeToTweet3 = true;
+
+	intervalCount++;
+
+	if(intervalCount >= 48) { //48 because runs every 30min * 48 = 24 hours
+		//end of the day, tweet weather status and reset
+		if(weatherCount >= 30) { //30 incidents of negative posting (regardless of whether I respond to it)
+			//lots of negativitely today
+			console.log('Weather: high');
+			weather("high");
+		} else if(weatherCount >= 15) {
+			//some negativity today
+			console.log('Weather: med');
+			weather("med");
+		} else {
+			//mostly quiet
+			console.log('Weather: low');
+			weather("low");
+		}
+
+		intervalCount = 0;
+	}
 }
 
 
 runBot();
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(runBot, 1000 * 10);
+setInterval(runBot, 1000 * 60 * 30);
